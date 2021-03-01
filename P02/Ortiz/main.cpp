d69de5427d1bef1b6f8b9a4a8b78a2a45d838095
javierortiz21 <javierortiz@edu.uah.es>
2021-03-01 12:56:49 +0100

    Mejorado los throws

--- P02/Ortiz/main.cpp
+++ P02/Ortiz/main.cpp
@@ -1,19 +1,22 @@
 // Bibliotecas:
 #include <fstream>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main(int argc, char** argv) {
     int ret = 0;
     FlyCapture2::Error error;
     FlyCapture2::PGRGuid guid;
+
     // Encapsulo los errores con try y catch:
     try
     {
         // Compruebo los permisos:
         FILE* tempFile = fopen("test.txt", "w+");
         if (tempFile == NULL) 
-            throw("Cant_open_file");
+            throw string("Check_permissions :: Failed to create file");
 
         fclose(tempFile);
         remove("test.txt");
@@ -25,10 +28,11 @@ int main(int argc, char** argv) {
         
         // Si no hay camaras conectadas lanzo una excepción
         if (error != FlyCapture2::PGRERROR_OK) 
-            throw("Error leyendo la cámara");
+            throw string("Camera_error:: Not devices conected");
         
         cout<<"Number of cameras detected: " << numCameras<<endl;
 
+        
         // Enseño el frame de cada una de las cámaras
         for (unsigned int i = 0; i < numCameras; i++) {
             // Voy a subir esto pero no se si seguira funcionando:
@@ -37,28 +41,18 @@ int main(int argc, char** argv) {
             error = busMgr.GetCameraFromIndex(i, &guid);
             
             if (error != FlyCapture2::PGRERROR_OK)
-                throw("Error leyendo la cámara");
+                throw string("Camera_error:: Error in get camera index");
 
             // Lanzo la cámara:    
             runSingleCamera(guid,i);
+        }   
+
+    }
+    catch (string e)
+    {
+        cout << e << endl;
     }
 
-    catch("Cant_open_file")
-        printf("Failed to create file in current folder.  Please check permissions.\n");
 
-
-    catch("Error leyendo la cámara")
-        printError(error);
-
-    // Compruebo si tengo los permisos para abrir ficheros:
-    
-    else {
-        
-        // Leo el número de cámaras que estan conectadas:
-        // No es mejor hacer un try y un except
-        else {
-                        }
-        }
-    }
-    return ret;
+    return 0;
 }
